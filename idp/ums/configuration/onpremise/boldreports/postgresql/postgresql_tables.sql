7ac2d2739a2b5c5dc541c43d3ce616c47b895fc1
Silamabarasan <silambarasan.g@syncfusion.com>
2026-03-11 19:53:30 +0530

    x64bitchanges

--- idp/ums/configuration/onpremise/boldreports/postgresql/postgresql_tables.sql
+++ idp/ums/configuration/onpremise/boldreports/postgresql/postgresql_tables.sql
@@ -814,6 +814,27 @@ CREATE TABLE BOLDRS_UserSession(
 	LastActive timestamp NULL,
 	IsActive smallint NOT NULL)
 ;
+
+CREATE TABLE BOLDRS_EmailActivityLog(
+    Id  SERIAL PRIMARY KEY NOT NULL,
+    Event varchar(255) NOT NULL,
+    RecipientEmail varchar(255) NOT NULL,
+    SenderEmail varchar(255) NOT NULL,
+    MailSubject varchar(255) NOT NULL,
+    MailBody text NULL,
+    CreatedDate timestamp NOT NULL,
+    ModifiedDate timestamp  NULL,
+    InitiatedBy int NOT NULL,
+    UserId int NULL,
+    GroupId int NULL,
+    ItemId uuid NULL,
+    CommentId int NULL,
+    PermissionId int NULL,
+    Status int NOT NULL,
+    StatusMessage text NULL,
+    IsActive smallint NOT NULL)
+;
+
 	
 ---- PASTE INSERT Queries below this section --------
 
@@ -2064,6 +2085,15 @@ ALTER TABLE BOLDRS_ReportPartLinkage  ADD FOREIGN KEY(ReportId) REFERENCES BOLDR
 ALTER TABLE BOLDRS_ReportPartLinkage ADD FOREIGN KEY(CreatedById) REFERENCES BOLDRS_User (Id)
 ;
 ALTER TABLE BOLDRS_ReportPartLinkage  ADD FOREIGN KEY(ModifiedById) REFERENCES BOLDRS_User (Id)
+;
+
+ALTER TABLE BOLDRS_EmailActivityLog  ADD  FOREIGN KEY(UserId) REFERENCES BOLDRS_User (Id)
+;
+ALTER TABLE BOLDRS_EmailActivityLog  ADD  FOREIGN KEY(GroupId) REFERENCES BOLDRS_Group (Id)
+;
+ALTER TABLE BOLDRS_EmailActivityLog  ADD  FOREIGN KEY(ItemId) REFERENCES BOLDRS_Item (Id)
+;
+ALTER TABLE BOLDRS_EmailActivityLog  ADD FOREIGN KEY(CommentId) REFERENCES BOLDRS_Comment (Id)
 ;
 
 ALTER TABLE BOLDRS_MultiTabReport  ADD FOREIGN KEY(ParentReportId) REFERENCES BOLDRS_Item (Id)
