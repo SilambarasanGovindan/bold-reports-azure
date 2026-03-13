85c370c06441eddaf6ae2f311ce869a4aeb95992
Silamabarasan <silambarasan.g@syncfusion.com>
2026-03-13 19:22:30 +0530

    deleted the files

--- idp/web/configuration/onpremise/boldid/mysql/versioned_scripts/latest.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE {database_name}.BOLDTC_Tenant ADD ProxyFullPathUrl char(255) NULL;
-
-ALTER TABLE {database_name}.BOLDTC_Tenant ADD PreventProxyDomainAutoUpdate tinyint(1) NOT NULL DEFAULT 0;
